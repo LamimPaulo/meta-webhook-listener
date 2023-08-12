31b4827811b1c37600db601405dd2b8687bcf26a
Glitch (messenger-platform-quick-start) <none>
2023-08-12 00:26:30 -0300

    feat:: send data on amqp

--- app.js
+++ app.js
@@ -41,6 +41,16 @@ app.post("/webhook", (req, res) => {
         },
         headers: { "Content-Type": "application/json" },
       });
+
+      var jackrabbit = require('jackrabbit');
+      var url = process.env.CLOUDAMQP_URL || "amqp://localhost";
+      var rabbit = jackrabbit(url).
+
+      rabbit
+        .default()
+        .publish({ msg: msg_body }, { key: 'wp-hook' })
+        .on('drain', rabbit.close);
+
     }
     res.sendStatus(200);
   } else {
@@ -49,17 +59,23 @@ app.post("/webhook", (req, res) => {
 });
 
 app.get("/webhook", (req, res) => {
+
   const verify_token = process.env.VERIFY_TOKEN;
 
+  // Parse params from the webhook verification request
   let mode = req.query["hub.mode"];
   let token = req.query["hub.verify_token"];
   let challenge = req.query["hub.challenge"];
 
+  // Check if a token and mode were sent
   if (mode && token) {
+    // Check the mode and token sent are correct
     if (mode === "subscribe" && token === verify_token) {
+      // Respond with 200 OK and challenge token from the request
       console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
     } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);
     }
   }
